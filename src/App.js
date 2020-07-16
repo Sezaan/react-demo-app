5d8e1d4c228836de78dc953acbd24fd001a59eff
root <root@Sezan-PC.localdomain>
2020-07-16 12:44:13 +0600

    test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,13 +6,43 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit shesh <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>How may we help you?</h1>
+        <Counter />
       </header>
     </div>
   );
+}
+
+function Product(props) {
+  const productStyle = {
+    border: '1px solid gray',
+    borderRadius: '5px',
+    backgroundColor: 'lightgray',
+    height: '200px',
+    width: '200px',
+    float: 'left'
+  };
+  return (
+    <div style={productStyle}>
+      <h2>Name: </h2>
+      <h1></h1>
+      <button>Buy Now</button>
+    </div>
+  )
+}
+
+function Counter() {
+  const [count, setCount] = useState(69);
+  const handleIncrease = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+  };
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={handleIncrease}>Increase</button>
+    </div>
+  )
 }
 
 export default App;
